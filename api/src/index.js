f267291c8c2f5cfe1171a1185e299311c5af2e5b
aaron-dm-mcdonald <mcdonald.dm.aaron@gmail.com>
2025-03-06 15:48:22 -0800

    update index.js with DB schema

--- api/src/index.js
+++ api/src/index.js
@@ -1,22 +1,33 @@
-const express = require('express');
-const {port, host, db} = require("./configuration");
-const {connectDb} = require("./helpers/db");
+const express = require("express");
+const mongoose = require("mongoose");
+const { port, host, db } = require("./configuration");
+const { connectDb } = require("./helpers/db");
 
 const app = express();
+const kittySchema = new mongoose.Schema({
+  name: String
+});
+const Kitten = mongoose.model("Kitten", kittySchema);
 
-app.get('/test', (req, res) => {
-    res.send('==> our api server is working fine');
+app.get("/test", (req, res) => {
+  res.send("Our api server is working correctly");
 });
 
 const startServer = () => {
-    app.listen(port, async () => {
-        console.log(`==> Server api started on port ${port}`);
-        console.log(`==> Our host is ${host}`);
-        console.log(`==> Our db is ${db}`);
+  app.listen(port, () => {
+    console.log(`Started api service on port ${port}`);
+    console.log(`Our host is ${host}`);
+    console.log(`Database url ${db}`);
+
+    const silence = new Kitten({ name: "Silence" });
+    silence.save(function(err, result) {
+      if (err) return console.error(err);
+      console.log("result", result);
     });
-}
+  });
+};
 
 connectDb()
-    .on("error", console.log)
-    .on("disconnected", connectDb)
-    .once("open", startServer);
+  .on("error", console.log)
+  .on("disconnected", connectDb)
+  .once("open", startServer);
