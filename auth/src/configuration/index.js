a0ac8bb1f3af839ab325f5ac5c3cdaed3628a7ae
aaron-dm-mcdonald <mcdonald.dm.aaron@gmail.com>
2025-04-02 16:00:09 -0700

    add axios and test endpoint to auth service

--- auth/src/configuration/index.js
+++ auth/src/configuration/index.js
@@ -1,3 +1,4 @@
 module.exports.port = process.env.PORT || 3000;
 module.exports.host = process.env.HOST || 'localhost';
 module.exports.db = process.env.MONGO_URL;
+module.exports.apiUrl = process.env.API_URL;
